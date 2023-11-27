72e1b6fef4f182fe8541a8e3ea8adbd2685a45b8
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-11-27 20:22:15 +0100

    Implement new config

---
+++ src/NewConfig.cpp
@@ -0,0 +1,48 @@
+﻿#include "NewConfig.h"
+#include "helpers/io_helpers.h"
+#define CONFIG_PATH "newconfig.bin"
+
+constexpr t_config default_config;
+t_config new_config;
+
+
+void save_config()
+{
+    printf("Saving config...\n");
+    FILE* f = fopen(CONFIG_PATH, "wb");
+    if (!f)
+    {
+        printf("Can't save config\n");
+        return;
+    }
+    fwrite(&new_config, sizeof(t_config), 1, f);
+    fclose(f);
+}
+
+void load_config()
+{
+    printf("Loading config...\n");
+
+    auto buffer = read_file_buffer(CONFIG_PATH);
+    t_config loaded_config;
+
+    if (buffer.empty())
+    {
+        // Failed, reset to default
+        printf("No config found, using default\n");
+        loaded_config = default_config;
+    } else
+    {
+        uint8_t* ptr = buffer.data();
+        memread(&ptr, &loaded_config, sizeof(t_config));
+    }
+    
+    if (loaded_config.version < default_config.version)
+    {
+        // Outdated version, reset to default
+        printf("Outdated config version, using default\n");
+        loaded_config = default_config;
+    }
+
+    new_config = loaded_config;
+}
---
+++ src/NewConfig.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <cstdint>
+
+typedef struct s_config
+{
+    int32_t version = 0;
+    int32_t always_on_top = false;
+    int32_t float_from_parent = true;
+} t_config;
+
+extern t_config new_config;
+
+/**
+ * \brief Saves the current config to a file
+ */
+void save_config();
+
+/**
+ * \brief Loads the config from a file
+ */
+void load_config();
+
