333dfde6e99856e2183f6ff0d10e0ebc246be0ec
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-02-12 12:32:22 +0100

    Chore/Dev: Reduce indentation in Combo.cpp

--- src/Combo.cpp
+++ src/Combo.cpp
@@ -7,70 +7,67 @@
 #include "stdafx.h"
 #include "Combo.h"
 
-namespace Combos
+std::vector<Combos::Combo*> Combos::find(const char* path)
 {
-    std::vector<Combo*> find(const char* path)
+    FILE* f = fopen(path, "rb");
+
+    if (!f)
     {
-        FILE* f = fopen(path, "rb");
+        return {};
+    }
 
-        if (!f)
+    std::vector<Combos::Combo*> combos;
+    char current = fgetc(f);
+
+    while (current != -1)
+    {
+        auto combo = new Combos::Combo();
+
+        std::string name;
+
+        while (current != '\0')
         {
-            return {};
-        }
-
-        std::vector<Combo*> combos;
-        char current = fgetc(f);
-
-        while (current != -1)
-        {
-            auto combo = new Combo();
-
-            std::string name;
-
-            while (current != '\0')
-            {
-                name.push_back(current);
-                current = fgetc(f);
-            }
-
-            combo->name = name;
-
-            int32_t sample_count = 0;
-            fread(&sample_count, sizeof(int32_t), 1, f);
-
-            combo->samples = {};
-            combo->samples.resize(sample_count);
-
-            fread(combo->samples.data(), sizeof(uint32_t), sample_count, f);
-
+            name.push_back(current);
             current = fgetc(f);
-            combos.push_back(combo);
         }
 
-        return combos;
+        combo->name = name;
+
+        int32_t sample_count = 0;
+        fread(&sample_count, sizeof(int32_t), 1, f);
+
+        combo->samples = {};
+        combo->samples.resize(sample_count);
+
+        fread(combo->samples.data(), sizeof(uint32_t), sample_count, f);
+
+        current = fgetc(f);
+        combos.push_back(combo);
     }
 
-    bool save(const char* path, std::vector<Combo*> combos)
+    return combos;
+}
+
+bool Combos::save(const char* path, std::vector<Combo*> combos)
+{
+    FILE* f = fopen(path, "wb");
+
+    if (!f)
     {
-        FILE* f = fopen(path, "wb");
-
-        if (!f)
-        {
-            return false;
-        }
-
-        for (auto& combo : combos)
-        {
-            fputs(combo->name.c_str(), f);
-            // null-terminate the string
-            fputc(0, f);
-
-            uint32_t size = combo->samples.size();
-            fwrite(&size, 4, 1, f);
-            fwrite(combo->samples.data(), sizeof(BUTTONS), combo->samples.size(), f);
-        }
-
-        fclose(f);
-        return true;
+        return false;
     }
-}; // namespace Combos
+
+    for (auto& combo : combos)
+    {
+        fputs(combo->name.c_str(), f);
+        // null-terminate the string
+        fputc(0, f);
+
+        uint32_t size = combo->samples.size();
+        fwrite(&size, 4, 1, f);
+        fwrite(combo->samples.data(), sizeof(BUTTONS), combo->samples.size(), f);
+    }
+
+    fclose(f);
+    return true;
+}
