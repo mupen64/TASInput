b486b949dda7dbed083d300a47214454cb9f30dc
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-02-12 13:09:12 +0100

    TASInput/Dev: Move some more non-global macros into implementation file

--- src/TASInput.h
+++ src/TASInput.h
@@ -8,17 +8,6 @@
 
 #define SUBKEY "Software\\N64 Emulation\\DLL\\TASDI"
 
-#define IDT_TIMER_STATUS_0 5000
-#define IDT_TIMER_STATUS_1 5001
-#define IDT_TIMER_STATUS_2 5002
-#define IDT_TIMER_STATUS_3 5003
-
-#define C_IDLE 0
-#define C_PLAY 1
-#define C_RECORD 4
-
-#define EDIT_END 10001
-
 void WINAPI InitializeAndCheckDevices(HWND hMainWindow);
 LRESULT CALLBACK StatusDlgProc(HWND hDlg, UINT Message, WPARAM wParam, LPARAM lParam);
 VOID CALLBACK StatusDlgProcTimer(UINT idEvent, UINT uMsg, DWORD dwUser, DWORD dw1, DWORD dw2);
