ced470a9381c148635ba502882fd3b0b322b267a
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-04-26 17:31:02 +0200

    chore: add missing copyright headers

--- src/Main.h
+++ src/Main.h
@@ -1,4 +1,10 @@
-﻿#pragma once
+﻿/*
+ * Copyright (c) 2025, TASInput maintainers, contributors, and original authors (nitsuja, Deflection).
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
 
 extern HINSTANCE g_inst;
 extern core_plugin_extended_funcs* g_ef;
