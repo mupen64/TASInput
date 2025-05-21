eba019fe8517e568a6c50b190e732eb6976cb302
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-05-21 09:43:46 +0200

    chore: adhere to plugin naming guidelines

--- src/Main.h
+++ src/Main.h
@@ -8,21 +8,19 @@
 
 extern HINSTANCE g_inst;
 extern core_plugin_extended_funcs* g_ef;
-extern int MOUSE_LBUTTONREDEFINITION;
-extern int MOUSE_RBUTTONREDEFINITION;
 
 #define PLUGIN_VERSION L"2.0.0-rc1"
 
 #ifdef _M_X64
-#define PLUGIN_ARCH L"-x64"
+#define PLUGIN_ARCH L" x64"
 #else
-#define PLUGIN_ARCH L"-x86"
+#define PLUGIN_ARCH L" x86"
 #endif
 
 #ifdef _DEBUG
-#define PLUGIN_TARGET L"-debug"
+#define PLUGIN_TARGET L" Debug"
 #else
-#define PLUGIN_TARGET L"-release"
+#define PLUGIN_TARGET L" Release"
 #endif
 
 #define PLUGIN_NAME L"TASInput " PLUGIN_VERSION PLUGIN_ARCH PLUGIN_TARGET
