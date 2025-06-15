5569bc42b274de07ff2d69957166a742f28d00c6
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-06-15 20:36:56 +0200

    refactor: use registry to store new config

--- src/NewConfig.cpp
+++ src/NewConfig.cpp
@@ -9,42 +9,63 @@
 #include <MiscHelpers.h>
 #include <Main.h>
 
-#define CONFIG_PATH "tasinput.cfg"
+#define CONFIG_VALUE L"Config"
 
-constexpr t_config default_config;
-t_config new_config;
-
+constexpr t_config default_config{};
+t_config new_config{};
 
 void save_config()
 {
     g_ef->log_trace(L"Saving config...");
-    FILE* f = fopen(CONFIG_PATH, "wb");
-    if (!f)
+
+    HKEY h_key{};
+
+    if (RegCreateKeyEx(HKEY_CURRENT_USER, SUBKEY, 0, NULL, 0, KEY_WRITE, NULL, &h_key, NULL) != ERROR_SUCCESS)
     {
-        g_ef->log_error(L"Couldn't save config");
+        g_ef->log_error(L"RegCreateKeyEx failed");
         return;
     }
-    fwrite(&new_config, sizeof(t_config), 1, f);
-    fclose(f);
+
+    if (RegSetValueEx(h_key, CONFIG_VALUE, 0, REG_BINARY, reinterpret_cast<const BYTE*>(&new_config), sizeof(t_config)) != ERROR_SUCCESS)
+    {
+        g_ef->log_error(L"RegSetValueEx failed");
+        RegCloseKey(h_key);
+        return;
+    }
+
+    RegCloseKey(h_key);
 }
 
 void load_config()
 {
     g_ef->log_trace(L"Loading config...");
 
-    auto buffer = read_file_buffer(CONFIG_PATH);
-    t_config loaded_config;
+    HKEY h_key{};
+    DWORD size = sizeof(t_config);
 
-    if (buffer.empty() || buffer.size() != sizeof(t_config))
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, SUBKEY, 0, KEY_READ, &h_key) != ERROR_SUCCESS)
     {
-        g_ef->log_trace(L"No config found, using default");
-        loaded_config = default_config;
+        g_ef->log_error(L"RegCreateKeyEx failed");
+        return;
     }
-    else
+
+    t_config loaded_config{};
+
+    if (RegQueryValueEx(
+        h_key,
+        CONFIG_VALUE,
+        nullptr,
+        nullptr,
+        reinterpret_cast<BYTE*>(&loaded_config),
+        &size) != ERROR_SUCCESS ||
+        size != sizeof(t_config))
     {
-        uint8_t* ptr = buffer.data();
-        memread(&ptr, &loaded_config, sizeof(t_config));
+        g_ef->log_error(L"RegQueryValueEx failed");
+        RegCloseKey(h_key);
+        return;
     }
+
+    RegCloseKey(h_key);
 
     if (loaded_config.version < default_config.version)
     {
