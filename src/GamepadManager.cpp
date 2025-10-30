d708eff857929f41442413e7bc45dccbd7b85cc2
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-10-30 09:23:55 +0100

    fix joystick input

--- src/GamepadManager.cpp
+++ src/GamepadManager.cpp
@@ -17,18 +17,20 @@ static gamepad_manager_context g_ctx;
 
 static int32_t remap_axis(int16_t value, const bool is_y_axis)
 {
-    value = std::clamp(value, static_cast<int16_t>(-32768), static_cast<int16_t>(32767));
-
     const int32_t min_target = is_y_axis ? -127 : -128;
     const int32_t max_target = is_y_axis ? 128 : 127;
 
-    int32_t mapped = static_cast<int32_t>(value) * max_target / 32767;
+    const int32_t mapped = static_cast<int32_t>(value) * max_target / 32767;
 
-    if (value == -32768)
+    if (is_y_axis)
     {
-        mapped = min_target;
+        g_ef->log_trace(std::format(L"y: {}", std::clamp(mapped, min_target, max_target)).c_str());
     }
-
+    else
+    {
+        g_ef->log_trace(std::format(L"x: {}", std::clamp(mapped, min_target, max_target)).c_str());
+    }
+    
     return std::clamp(mapped, min_target, max_target);
 }
 
@@ -85,7 +87,7 @@ core_buttons GamepadManager::get_input()
     buttons.dr = SDL_GetGamepadButton(g_ctx.gamepad, SDL_GAMEPAD_BUTTON_DPAD_RIGHT);
 
     buttons.x = remap_axis(SDL_GetGamepadAxis(g_ctx.gamepad, SDL_GAMEPAD_AXIS_LEFTX), false);
-    buttons.y = remap_axis(SDL_GetGamepadAxis(g_ctx.gamepad, SDL_GAMEPAD_AXIS_LEFTY), true);
+    buttons.y = -remap_axis(SDL_GetGamepadAxis(g_ctx.gamepad, SDL_GAMEPAD_AXIS_LEFTY), true);
 
     return buttons;
 }
